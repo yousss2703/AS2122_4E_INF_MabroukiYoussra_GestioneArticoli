0292a69d801a04ea523f4b8861590ef2212dfff0
Youssra Mabrouki <2703mabrouki@gmail.com>
2022-05-18 09:13:56 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_MabroukiYoussra_GestioneArticoli/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS2122_4E_INF_MabroukiYoussra_GestioneArticoli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
